ea914cf6a75b9ae1a12b9fb7402b9d854c596a12
kedzkiest <kaede12kazu22@gmail.com>
2022-11-02 21:17:00 +0900

    added animation to the character

--- Reproduce_HeavenBurnsRed_Walking/Assets/C#/PlayerController.cs
+++ Reproduce_HeavenBurnsRed_Walking/Assets/C#/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotateSpeed;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
@@ -21,38 +22,68 @@ public class PlayerController : MonoBehaviour
             z: path.m_Waypoints[0].position.z);
 
         transform.rotation = Quaternion.identity;
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Rotate();
         Move();
     }
 
+    [HideInInspector]
+    public Quaternion rot;
+
     private void Rotate()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.rotation = cart.transform.rotation;
+            rot = cart.transform.rotation;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.rotation = cart.transform.rotation * Quaternion.Euler(0, 180, 0);
+            rot = cart.transform.rotation * Quaternion.Euler(0, 180, 0);
         }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotateSpeed * Time.deltaTime);
     }
 
     private void Move()
     {
+        transform.position = new Vector3(
+            x: transform.position.x,
+            y: 0,
+            z: transform.position.z);
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            if (!path.m_Looped && Mathf.Abs(path.PathLength - cart.m_Position) < 0.1f)
+            {
+                anim.SetBool("isRunning", false);
+                return;
+            }
+
             cart.m_Position += moveSpeed * Time.deltaTime;
+            anim.SetBool("isRunning", true);
+            return;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            if (!path.m_Looped && Mathf.Abs(cart.m_Position) < 0.1f)
+            {
+                anim.SetBool("isRunning", false);
+                return;
+            }
+
             cart.m_Position -= moveSpeed * Time.deltaTime;
+            anim.SetBool("isRunning", true);
+            return;
         }
+
+        anim.SetBool("isRunning", false);
     }
 }
