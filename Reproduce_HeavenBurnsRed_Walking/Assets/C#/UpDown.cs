0772d097ebe92348c281a1e2223235831f17bcfb
kedzkiest <kaede12kazu22@gmail.com>
2022-11-30 11:13:57 +0900

    completed trail winding effect using spline

--- Reproduce_HeavenBurnsRed_Walking/Assets/C#/UpDown.cs
+++ Reproduce_HeavenBurnsRed_Walking/Assets/C#/UpDown.cs
@@ -6,34 +6,26 @@ public class UpDown : MonoBehaviour
 {
     private GameObject player;
     [SerializeField] private float playerInteractionDist;
-    [SerializeField] private float yOffset;
     private Vector3 origin;
-    private SplineInstantiate splineInstantiate;
     private float distToPlayer;
     private Vector3 apparentPosition;
+    [SerializeField] Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        splineInstantiate = GameObject.FindGameObjectWithTag("SplineInstantiate").GetComponent<SplineInstantiate>();
         origin = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        apparentPosition = transform.position - splineInstantiate.maxPositionOffset;
+        apparentPosition = origin + offset;
         distToPlayer = (player.transform.position - apparentPosition).magnitude;
-        Debug.DrawRay(apparentPosition, player.transform.position - apparentPosition);
 
-        if(distToPlayer < playerInteractionDist)
-        {
-            transform.position = origin + new Vector3(0, -yOffset, 0);
-        }
-        else
-        {
-            transform.position = origin;
-        }
+        float depth = distToPlayer - playerInteractionDist;
+        if (depth > 0) return;
+        transform.position = origin + new Vector3(0, depth, 0);
     }
 }
